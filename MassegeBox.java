3456f2a08370d7d6b3bd7e7ea397f539f200d0f9
Mohsin I Amin <48616524+MohsinIAmin@users.noreply.github.com>
2019-09-20 16:32:36 +0600

    Add files via upload

--- MassegeBox.java
+++ MassegeBox.java
@@ -12,12 +12,18 @@ public class MassegeBox {
     double inputBoxDouble(String showMassege)
     {
         String input=JOptionPane.showInputDialog(showMassege);
+        if("".equals(input)){
+            return -1;
+        }
         double number=Double.parseDouble(input);
         return number;
     }
     int inputBoxInt(String showMassege)
-    {
+    {        
         String input=JOptionPane.showInputDialog(showMassege);
+        if("".equals(input)){
+            return -1;
+        }
         int number=Integer.parseInt(input);
         return number;
     }
